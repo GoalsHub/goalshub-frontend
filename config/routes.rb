a1f1f2e8360fbe5df1f16b402ff6ef85aedee552
Vladimir Legkunets <vladimir.legkunets@gmail.com>
2015-07-11 19:58:39 +0300

    Adding domain model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  resources :steps
-  resources :stages
-  resources :plans
+
   devise_for :users
+
   resources :goals
+  resources :plans
+  resources :stages
+  resources :steps
+
   resources :tokens,:only => [:create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
