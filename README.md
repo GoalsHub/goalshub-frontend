ab744e7ef865842f72b4fbac595bd5acf24b5bd1
Vladimir Legkunets <vladimir.legkunets@gmail.com>
2015-07-11 19:43:40 +0300

    Using postgres.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # goalshub-frontend
+
+## Postgress
+
+### Database creation
+
+    psql --user=postgres --port=5432 --host=localhost
+
+    create user goalshub_dev with createdb password 'goalshub_dev';
+    create user goalshub_test with createdb password 'goalshub_test';
+    create user goalshub_prod with createdb password 'goalshub_prod';
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150711143619) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "goals", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -37,7 +40,7 @@ ActiveRecord::Schema.define(version: 20150711143619) do
     t.string   "authentication_token"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
