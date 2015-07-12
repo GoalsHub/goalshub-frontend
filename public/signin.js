eb2069e5772ab3aaf82a128bb5dc05429ddc8363
pavelpower <pavel.power@gmail.com>
2015-07-12 08:58:39 +0300

    added panel-tree.html to public

---
+++ public/signin.js
@@ -0,0 +1,28 @@
+$('#form-signin').submit(function (event) {
+    var email = $('#email').val(),
+       password = $('#password').val();
+
+    $.post('http://sleepy-castle-1003.herokuapp.com/tokens.json', {
+       'email': email,
+       'password': password
+    }).done(function(data) {
+
+        sessionStorage.setItem('email', email);
+        sessionStorage.setItem('token', data.token);
+
+        window.location = 'panel-tree.html';
+
+    }).fail(function (data) {
+        var modal = $('#modal-error-singin');
+
+        if (data.responseJSON && data.responseJSON.message) {
+            modal.find('.modal-content').html('<p>' + data.responseJSON.message + '</p>');
+        }
+
+        modal.modal();
+    });
+
+    event.preventDefault();
+
+    return false;
+});
